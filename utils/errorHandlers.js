4e1eb7dda15522402eb45adab5ecef1724eeb9b7
Chidi Orji <orjichidi95@gmail.com>
2019-05-15 15:42:27 +0100

    chore(error handling): Minor non-breaking changes

--- utils/errorHandlers.js
+++ utils/errorHandlers.js
@@ -1,17 +1,7 @@
-import { dev_logger } from '../utils/loggers';
-
 export const InternalServerError = (req, res, e) => {
-    dev_logger(`Location: ${req.url}\nError message: ${e.toString()}`);
     return res.status(500).json({
         reason: e.toString(),
         error: e,
         message: 'Internal server error',
-    });
-};
-
-export const CriticalError = (res, error) => {
-    return res.status(500).json({
-        error,
-        message: 'Something went wrong. Please try again'
     });
 };
