ee091b3937789e25d596d6d1acae7263a27bd6dd
Chidi Orji <orjichidi95@gmail.com>
2019-05-18 13:55:48 +0100

    feature(dashboard): make profile picture editable
    - User can change profile in dashboard by clicking (1MB max)
    - refactor user information display
    - Show address only on dashboard and admin view of user page
    - Display user info using flexbox for small screens.
    - Added additional breakpoint of 1500px to properly display user info
    - Remove all border radius from divs
    - show border around get started button
    
    [Finishes #166084608]

--- UI/js/dashboard.js
+++ UI/js/dashboard.js
@@ -1,14 +1,52 @@
-const edit_profile_modal = document.getElementById('edit_profile_modal');
-const loan_application_modal = document.getElementById(
-    'loan_application_modal'
-);
 
-// Close modal if user clicks outside modal
-window.onclick = function(e) {
-    if (e.target === edit_profile_modal) {
-        edit_profile_modal.style.display = 'none';
+const profile_pix = document.getElementById('profile_pix');
+const img_uploader = document.querySelector('input[id=img_uploader]');
+
+profile_pix.addEventListener('click', e => {
+    e.preventDefault();
+    img_uploader.click();
+});
+
+const update_profile_pix = file => {
+    const reader  = new FileReader();
+    reader.addEventListener('load', () => {
+        const { result } = reader;
+        profile_pix.src = result;
+    });
+    if (file) {
+        reader.readAsDataURL(file);
     }
-    else if (e.target === loan_application_modal) {
-        loan_application_modal.style.display = 'none';
+    else {
+        profile_pix.src = './img/chidi.jpg';
     }
 };
+
+img_uploader.onchange = e => {
+    e.preventDefault();
+    const [ file ] = e.target.files;
+    const fsize = file.size;
+    const size_in_mb = (fsize / (1024 * 1024)).toFixed(2);
+
+    if (fsize > 1024 * 1024 * 1) {
+        alert(`File is ${size_in_mb}MB. Allowed size is 1MB.`);
+        return;
+    }
+    update_profile_pix(file);
+    // upload photo to AWS S3
+    // get a presigned url with post request POST /users/presigned_url
+    // upload to S3
+    // save file path to database POST /users/:id/:aws_url
+};
+
+const url = 'http://localhost:3000/users';
+
+// eslint-disable-next-line no-undef
+axios
+    .get(url)
+    .then((response) => {
+        const { data } = response;
+        console.log(data);
+    })
+    .catch((error) => {
+        console.log(error);
+    });
