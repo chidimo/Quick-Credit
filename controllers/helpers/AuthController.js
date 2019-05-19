f487093417aaca5351d391532cc43295e618f627
Chidi Orji <orjichidi95@gmail.com>
2019-05-19 01:15:13 +0100

    refactor(authentication): make functions reusable
    - Create helper functions for modularity
    - function check user existence
    - function to add user to db
    - function to get user by id or email

--- controllers/helpers/AuthController.js
+++ controllers/helpers/AuthController.js
@@ -1,50 +1,43 @@
 import bcrypt from 'bcrypt';
 
 import { InternalServerError } from '../../utils/errorHandlers';
-import { dev_logger } from '../../utils/loggers';
 
-export const check_user_existence = async (model_instance, req, res) => {
-    const { email } = req.body;
+export const check_user_existence = async (model_instance, email, res) => {
     try {
         const { rows } = await model_instance.select(
             'id, email', `WHERE email='${email}'`);
         const [ user, ] = rows;
+        
         if (user) return user;
     }
-    catch (e) { return InternalServerError(req, res, e);}
+    catch (e) { return InternalServerError(null, res, e);}
 };
 
-export const create_user = async (model_instance, req, res) => {
-    const { email, password } = req.body;
+export const add_user_to_db = async (model_instance, req, res) => {
+    const { email, password, firstname, lastname } = req.body;
     const hashedPassword = bcrypt.hashSync(password, 8);
 
     try {
-        await model_instance.insert('(email, password)',
-            `'${email}', '${hashedPassword}'`);                    
+        return await model_instance.insert_with_return(
+            '(email, firstname, lastname, password)',
+            `'${email}', '${firstname}', '${lastname}', 
+            '${hashedPassword}'`
+        );                    
     }
     catch (e) { return InternalServerError(req, res, e);}
 };
 
-export const get_user = async (model_instance, req, res, code) => {
-    const { email } = req.body;
-
+export const get_user = async (model_instance, res, clause, err_msg) => {
     try {
         const { rows } = await model_instance.select(
-            `id, email, password, firstname,
+            `id, email, firstname,
             lastname, phone, status, address`,
-            `WHERE email='${email}'`
+            clause
         );
-        const [ user, ] = rows;
-        if (!user) {
-            return res
-                .status(404)
-                .json({ error: `User with email ${email} not found` });
-        }
-        // token verification takes care of identifying the user
-        dev_logger(`conde: ${code}, type: ${typeof code}`);
-        return res
-            .status(code)
-            .json({ data: { ...rows[0], token: req.token } });
+        if (rows.length === 0) return res.status(404).json({ 
+            error: err_msg
+        });
+        return rows[0];
     }
-    catch (e) { return InternalServerError(req, res, e); }
+    catch (e) { return InternalServerError(null, res, e); }
 };
