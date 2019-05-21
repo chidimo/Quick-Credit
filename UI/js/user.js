0b250d2399769a52ec8eefd4d0ad1a845917a3e9
Chidi Orji <orjichidi95@gmail.com>
2019-05-21 21:54:30 +0100

    chore(UI js): improve code coverage
    - configure codeclimate reporting

--- UI/js/user.js
+++ UI/js/user.js
@@ -1,32 +1,31 @@
 const verify_user = document.getElementById('verify_user');
 const un_verify_user = document.getElementById('un_verify_user');
 
+const promptUser = (text, defaultText, msg) => {
+    const cont = prompt(text, defaultText);
+    if (cont !== null) {
+        alert(msg);
+    }
+};
+
 verify_user.addEventListener('click', e => {
     e.preventDefault();
-    const cont = prompt(
-        'Please confirm if you want to verify this user',
-        'Click okay to reject this loan request');
-    
-    if (cont !== null) {
-        // verify user
-        alert('User was verified.');
-        // Send email notification
-    }
 
+    promptUser(
+        'Confirm user verification',
+        'Click okay to verify this user',
+        'User was verified.'
+    );
     return;
 });
 
 un_verify_user.addEventListener('click', e => {
     e.preventDefault();
-    const cont = prompt(
-        "Please confirm if you want to verify revoke this user's verification",
-        "Click okay to revoke this user's verification");
-    
-    if (cont !== null) {
-        // revoke verification
-        alert("User's verification was revoked.");
-        // Send email notification
-    }
 
+    promptUser(
+        'Confirm revoking user verification',
+        "Click okay to revoke this user's verification",
+        "User's verification was revoked."
+    );
     return;
 });
